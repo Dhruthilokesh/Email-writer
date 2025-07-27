bf70b677de5c88adfc251717fc9008537937c1b3
Dhruthilokesh <153724694+Dhruthilokesh@users.noreply.github.com>
2025-07-27 11:01:02 +0530

    Add files via upload

---
+++ EmailWriterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.email.email_writer_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailWriterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
